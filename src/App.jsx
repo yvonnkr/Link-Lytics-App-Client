4df4db1b96d503c3d36ec73b9008c5c8f878b0e2
yvonnkr <yvonnkr86@gmail.com>
2025-01-10 18:25:31 +0000

    install and setup tailwind, add fonts, config prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import "./App.css"
+import "./App.css";
 
 function App() {
-
-    return (
-        <>
-            <h2>Linklytics App</h2>
-        </>
-    )
+  return (
+    <>
+      <div className="container">
+        <h2 className="text-3xl text-emerald-800 font-bold underline italic ">
+          LinkLytics App
+        </h2>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;

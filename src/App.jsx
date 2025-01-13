c32708b05a34f87e537d9c0f91947537f241d7cb
yvonnkr <yvonnkr86@gmail.com>
2025-01-13 00:35:20 +0000

    add register page, custom textfield component with react-hook-form

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AboutPage from "./components/AboutPage.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
+import RegisterPage from "./components/RegisterPage.jsx";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Routes>
         <Footer />
       </Router>
---
+++ src/components/RegisterPage.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import TextField from "./TextField";
+import { Link, useNavigate } from "react-router-dom";
+
+const RegisterPage = () => {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(false);
+
+  const formDefaultValues = {
+    username: "",
+    email: "",
+    password: "",
+  };
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({ defaultValues: formDefaultValues, mode: "onTouched" });
+
+  const registerHandler = async (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="min-h-[calc(100vh-64px)] flex justify-center items-center">
+      <form
+        onSubmit={handleSubmit(registerHandler)}
+        className="sm:w-[450px] w-[360px]  shadow-custom py-8 sm:px-8 px-4 rounded-md"
+      >
+        <h1 className="text-center font-serif text-btnColor font-bold lg:text-3xl text-2xl">
+          Register
+        </h1>
+
+        <hr className="mt-2 mb-5 text-black" />
+
+        <div className="flex flex-col gap-3">
+          <TextField
+            label="UserName"
+            required
+            id="username"
+            type="text"
+            message="*Username is required"
+            placeholder="Type your username"
+            register={register}
+            errors={errors}
+          />
+
+          <TextField
+            label="Email"
+            required
+            id="email"
+            type="email"
+            message="*Email is required"
+            placeholder="Type your email"
+            register={register}
+            errors={errors}
+          />
+
+          <TextField
+            label="Password"
+            required
+            id="password"
+            type="password"
+            message="*Password is required"
+            placeholder="Type your password"
+            register={register}
+            min={6}
+            errors={errors}
+          />
+        </div>
+
+        <button
+          disabled={loader}
+          type="submit"
+          className="bg-customRed font-semibold text-white  bg-custom-gradient w-full py-2 hover:text-slate-400 transition-colors duration-100 rounded-sm my-3"
+        >
+          {loader ? "Loading..." : "Register"}
+        </button>
+
+        <p className="text-center text-sm text-slate-700 mt-6">
+          Already have an account?
+          <Link className="font-semibold " to="/login">
+            <span className="text-btnColor hover:text-black"> Login </span>
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterPage;

81fda4780f7f4f8d6b4f7f1668ac310d5e7ed89d
yvonnkr <yvonnkr86@gmail.com>
2025-01-16 17:07:31 +0000

    install react-query, fetch analytics data from api

---
+++ src/hooks/useQuery.js
@@ -0,0 +1,56 @@
+import { useQuery } from "react-query";
+import api from "../api/api.js";
+
+export const useFetchMyShortUrls = (token, onError) => {
+  return useQuery(
+    "my-shortenurls",
+    async () => {
+      return await api.get("/api/urls/myurls", {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: "Bearer " + token,
+        },
+      });
+    },
+    {
+      select: (data) => {
+        // sort response  data
+        return data.data.sort(
+          (a, b) => new Date(b.createdDate) - new Date(a.createdDate)
+        );
+      },
+      onError,
+      staleTime: 5000,
+    }
+  );
+};
+
+export const useFetchTotalClicks = (token, onError) => {
+  return useQuery(
+    "url-totalclick",
+    async () => {
+      return await api.get(
+        "/api/urls/totalClicks?startDate=2024-01-01&endDate=2025-12-31",
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+    },
+    {
+      select: (data) => {
+        //convert response data ToArray
+        return Object.keys(data.data).map((key) => ({
+          clickDate: key,
+          count: data.data[key], // data.data[2024-01-01]
+        }));
+      },
+      onError,
+      staleTime: 5000,
+    }
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import "./index.css";
 import App from "./App.jsx";
 import { ContextProvider } from "./contex/contextApi.jsx";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
